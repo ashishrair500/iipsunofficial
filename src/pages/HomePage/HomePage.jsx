6cb3f55fe5e1311a6ad38dbf3b6ba3e208ace49e
ashishrair500 <100395176+ashishrair500@users.noreply.github.com>
2024-05-06 00:20:44 +0530

    Placement and Events Pages

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,7 +4,7 @@ import Navbar from '../../components/Navbar/Navbar';
 import { useSelector } from 'react-redux/es/hooks/useSelector';
 import Footer from '../../components/Footer/Footer';
 import { Link } from 'react-router-dom';
- 
+
 
 import { shallowEqual } from 'react-redux';
 
@@ -22,13 +22,13 @@ const HomePage = () => {
       <div>
 
         <Navbar />
-        <img src="../../../public/assets/h1_hero.png" loading="lazy" alt="IIPS Logo" className='main-img'/>
+        <img src="../../../public/assets/h1_hero.png" loading="lazy" alt="IIPS Logo" className='main-img' />
         <div className='home-container'>
           <div className='home-content'>
-           
+
 
             <div className='content-below-line'>
-            {/* <img src="../../../public/assets/h1_hero.png" loading="lazy" alt="IIPS Logo" /> */}
+              {/* <img src="../../../public/assets/h1_hero.png" loading="lazy" alt="IIPS Logo" /> */}
               {/* <div className='text-container'>
 
 
@@ -58,55 +58,53 @@ const HomePage = () => {
             </div>
 
 
-   <div class="grid-container">
+            <div class="grid-container">
 
-    <div class="offering">
-        <img src="../../../public/assets/icon1.png" alt="Offering 1"  />
-        <div className='features-caption'>
-              <h3>Explore study </h3>
-              <p>Explore Some study Materialvgh cghg </p>
-      </div>
-        
-  
-    </div>
-    <div class="offering">
-    <img src="../../../public/assets/icon2.png" alt="Offering 1"  />
-        <div className='features-caption'>
-              <h3>Explore fdgdtgdhd</h3>
-              <p>Explore Some study Materialjj</p>
-        </div>
-    </div>
+              <div class="offering">
+                <img src="../../../public/assets/icon1.png" alt="Offering 1" />
+                <Link className='features-caption' to={isLoggedIn ? '/dashboard' : '/login'}>
+                  <h3>Explore study </h3>
+                  <p>Explore Some study Materialvgh cghg </p>
+                </Link>
+              </div>
+              <div class="offering">
+                <img src="../../../public/assets/icon2.png" alt="Offering 1" />
+                <Link className='features-caption' to={isLoggedIn ? '/placement' : '/login'}>
+                  <h3>Placements</h3>
+                  <p>Explore Placement Materialvgh </p>
+                </Link>
+              </div>
 
 
-      <div class="offering">
-      <img src="../../../public/assets/icon3.png" alt="Offering 1"  />
-        <div className='features-caption'>
-              <h3>Explore ggdgg</h3>
-              <p>Explore Some study Materialkjhj.</p>
-        </div>
-      </div>
-      
-
-
-    </div>
+              <div class="offering">
+                <img src="../../../public/assets/icon3.png" alt="Offering 1" />
+                <Link className='features-caption' to={isLoggedIn ? '/Events' : '/login'}>
+                  <h3>Events in IIPs</h3>
+                  <p>Join to Explore innner you</p>
+                </Link>
+              </div>
 
 
 
-    <section>
-  <div class="section-container">
-    <div class="left-part">
-      <img src="../../../public/assets/about2.png" alt="IIPS Logo" />
-    </div>
-    <div class="right-part">
-      <h2>Take the next step toward your personal and professional goals with us.</h2>
-      <p>The automated process all your website tasks. Discover tools and techniques to engage effectively with vulnerable children and young people.</p>
-      <a href="#" class="btn">Join now for Free</a>
-    </div>
-  </div>
-</section>
+            </div>
 
 
-{/*          
+
+            <section>
+              <div class="section-container">
+                <div class="left-part">
+                  <img src="../../../public/assets/about2.png" alt="IIPS Logo" />
+                </div>
+                <div class="right-part">
+                  <h2>Take the next step toward your personal and professional goals with us.</h2>
+                  <p>The automated process all your website tasks. Discover tools and techniques to engage effectively with vulnerable children and young people.</p>
+                  <a href="#" class="btn">Join now for Free</a>
+                </div>
+              </div>
+            </section>
+
+
+            {/*          
             <div className='carousel'>
             
 
---
+++ src/pages/Placements/Placement.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Navbar from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
+import './Placement.css';
+const Placement = () => {
+  return (
+    <>
+       <Navbar />
+    <h2>Placements</h2>
+    <h1>PLacemetns</h1>
+    <Footer />
+    </>
+  );
+};
+
+export default Placement;
